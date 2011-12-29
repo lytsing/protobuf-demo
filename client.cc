486446c654cc757d6cec136a2f4679872694ae0a
Lytsing Huang <hlqing@gmail.com>
2011-12-29 17:15:19 +0800

    add error handle

--- client.cc
+++ client.cc
@@ -29,12 +29,18 @@
 #include "./demo.people.pb.h"
 
 #define HOSTNAME "localhost"
-#define PORT 8000
-#define MAXDATASIZE 4096
+#define PORT 8000           // the port client will be connecting to
+#define MAXDATASIZE 4096    // max number of bytes we can get at once
 
 using std::cout;
 using std::endl;
 using std::string;
+
+/* simple little function to write an error string and exit */
+static void err(const char* s) {
+    perror(s);
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char** argv) {
     int fd;
@@ -43,14 +49,22 @@ int main(int argc, char** argv) {
     struct hostent *he;
     struct sockaddr_in server;
 
-    he = gethostbyname(HOSTNAME);
-    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if ((he = gethostbyname(HOSTNAME)) == NULL) {
+        err("gethostbyname");
+    }
+
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        err("socket");
+    }
+
     bzero(&server, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
     server.sin_addr = *((struct in_addr *)he->h_addr);
 
-    connect(fd, (struct sockaddr *)&server, sizeof(struct sockaddr));
+    if (connect(fd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1) {
+        err("connect");
+    }
 
     string msg;
     demo::People to;
--- server.cc
+++ server.cc
@@ -26,18 +26,30 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <string>
 #include <iostream>
 
 #include "./demo.people.pb.h"
 
-#define PORT 8000
-#define MAXDATASIZE 4096
-#define BACKLOG 10
+#define PORT 8000           // the port users will be connecting to
+#define MAXDATASIZE 4096    // max number of bytes we can send at once
+#define BACKLOG 10          // how many pending connections queue will hold
 
 using std::cout;
 using std::endl;
 using std::string;
+
+void sigchld_handler(int s) {
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+/* simple little function to write an error string and exit */
+static void err(const char* s) {
+    perror(s);
+    exit(EXIT_FAILURE);
+}
 
 int main(int argc, char** argv) {
     int listenfd;
@@ -47,22 +59,39 @@ int main(int argc, char** argv) {
     struct sockaddr_in server;
     struct sockaddr_in client;
     socklen_t sin_size;
+    struct sigaction sa;
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        err("socket");
+    }
 
     int opt = SO_REUSEADDR;
-    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+        err("setsockopt");
+    }
 
     bzero(&server, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORT);
-    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_family = AF_INET;                // host byte order
+    server.sin_port = htons(PORT);              // short, network byte order
+    server.sin_addr.s_addr = htonl(INADDR_ANY); // automatically fill with my IP
 
-    bind(listenfd, (struct sockaddr *)&server, sizeof(struct sockaddr));
+    if (bind(listenfd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1) {
+        err("bind");
+    }
 
-    listen(listenfd, BACKLOG);
+    if (listen(listenfd, BACKLOG) == -1) {
+        err("listen");
+    }
 
-    while (1) {
+    sa.sa_handler = sigchld_handler;  // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        err("sigaction");
+    }
+
+
+    while (1) {     // main accept() loop
         sin_size = sizeof(struct sockaddr_in);
 
         connectfd = accept(listenfd, (struct sockaddr *)&client, &sin_size);
